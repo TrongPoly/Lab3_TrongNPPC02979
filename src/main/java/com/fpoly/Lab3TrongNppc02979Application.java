2c91339f78ffe4d448d2a447ba78d7c2c82a79ec
TrongPoly <trongnguyen32293@gmail.com>
2023-05-18 13:56:56 +0700

    Initial commit

---
+++ src/main/java/com/fpoly/Lab3TrongNppc02979Application.java
@@ -0,0 +1,13 @@
+package com.fpoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3TrongNppc02979Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3TrongNppc02979Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/fpoly/model/Student.java
@@ -0,0 +1,63 @@
+package com.fpoly.model;
+
+public class Student {
+	String name;
+	String email;
+	Double marks;
+	Boolean gender;
+	String faculty;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Double marks) {
+		this.marks = marks;
+	}
+
+	public Boolean getGender() {
+		return gender;
+	}
+
+	public void setGender(Boolean gender) {
+		this.gender = gender;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public Student(String name, String email, Double marks, Boolean gender, String faculty) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.marks = marks;
+		this.gender = gender;
+		this.faculty = faculty;
+	}
+
+	public Student() {
+		super();
+	}
+
+}
